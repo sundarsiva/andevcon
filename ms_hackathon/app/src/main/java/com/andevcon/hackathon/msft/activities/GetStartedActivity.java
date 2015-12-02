00ace709db8bdd600ad326cfc6d23b166d04f7c6
Sundar Siva <ssiva@trulia.com>
2015-12-01 18:02:23 -0800

    added MS azure authentication

---
+++ ms_hackathon/app/src/main/java/com/andevcon/hackathon/msft/activities/GetStartedActivity.java
@@ -0,0 +1,18 @@
+package com.andevcon.hackathon.msft.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.support.android.designlibdemo.R;
+
+public class GetStartedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_get_started);
+
+    }
+
+}

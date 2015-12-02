179364a66728fd4692c7b86f03772873e075d669
Sundar Siva <ssiva@trulia.com>
2015-12-01 20:00:30 -0800

    new client ids

--- ms_hackathon/app/src/main/java/com/andevcon/hackathon/msft/helpers/Constants.java
+++ ms_hackathon/app/src/main/java/com/andevcon/hackathon/msft/helpers/Constants.java
@@ -10,8 +10,8 @@ public class Constants {
 
     public static final String AUTHORITY_URL = "https://login.microsoftonline.com/common";
     // Update these two constants with the values for your application:
-    public static final String CLIENT_ID = "69e93947-ec7a-4ded-81c1-6fba2fb78ed7";
-    public static final String REDIRECT_URI = "http://localhost:8000";
-    public static final String MICROSOFT_GRAPH_API_ENDPOINT = "https://graph.microsoft.com/v1.0/";
+    public static final String CLIENT_ID = "e2238cc0-ec85-4f3a-a7ca-12172115110d";
+    public static final String REDIRECT_URI = "http://localhost";
+    public static final String MICROSOFT_GRAPH_API_ENDPOINT = "https://graph.microsoft.com/beta/";
     public static final String MICROSOFT_GRAPH_API_ENDPOINT_RESOURCE_ID = "https://graph.microsoft.com/";
 }
